48a3c43b505b15e96602191b50b12de768df1fb1
Liz Henderson <lhenderson303@gmail.com>
2017-09-01 15:01:53 -0600

    contact page cleanup

--- src/components/pages/ContactPage.js
+++ src/components/pages/ContactPage.js
@@ -20,22 +20,57 @@ class ContactPage extends React.Component {
                  onClick={e => e.stopPropagation()}>
               <ScrollText
                 className='cursor-scroll'>
-                <p className="center single-line"> 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍</p>
-                <a href="https://www.google.com/maps/place/3515+Ringsby+Ct,+Denver,+CO+80216/@39.7733508,-104.9828953,17z/data=!3m1!4b1!4m5!3m4!1s0x876c7903ec2bc8b1:0x5782fe9631c8fd61!8m2!3d39.7733508!4d-104.9807066?sa=X&ved=0ahUKEwitgtycvITWAhVT6Z8KHRlVA5EQ8gEIJzAA"
-                   className="center">
-                  <p> 3515 Ringsby Ct #308 </p>
-                  <p> DENVER, CO 80216 </p>
-                </a>
-                <p className="center single-line"> 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫</p>
-                <a href="mailto:hello@iheartwunderwerkz.com">
-                  <p> hello@iheartwunderwerkz.com </p>
-                </a>
-                <p className="center single-line">  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞</p>
-                <p>303.594.4990</p>
-                <p className="center single-line"> 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 </p>
-                <a href="https://www.instagram.com/wunder_werkz/">
-                  <p>@wunder_werkz</p>
-                </a>
+                <div className="info-group">
+                  <p className="center single-line">
+                    📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍
+                  </p>
+                  <p> Stop By </p>
+                  <a href="https://www.google.com/maps/place/3515+Ringsby+Ct,+Denver,+CO+80216/@39.7733508,-104.9828953,17z/data=!3m1!4b1!4m5!3m4!1s0x876c7903ec2bc8b1:0x5782fe9631c8fd61!8m2!3d39.7733508!4d-104.9807066?sa=X&ved=0ahUKEwitgtycvITWAhVT6Z8KHRlVA5EQ8gEIJzAA"
+                     className="center">
+                    <p> 3515 Ringsby Ct #308 <br/> DENVER, CO 80216 </p>
+                  </a>
+                </div>
+                <div className="info-group">
+                  <p className="center single-line">
+                    📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫
+                  </p>
+                  <p> send a note </p>
+                  <a href="mailto:hello@iheartwunderwerkz.com">
+                    <p> hello@iheartwunderwerkz.com </p>
+                  </a>
+                </div>
+                <div className="info-group">
+                  <p className="center single-line">
+                     📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞
+                  </p>
+                  <p> drop a line </p>
+                  <p>303.594.4990</p>
+                </div>
+                <div className="info-group">
+                  <p className="center single-line">
+                    📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷
+                  </p>
+                  <p> peep the grams </p>
+                  <a href="https://www.instagram.com/wunder_werkz/">
+                    <p>@wunder_werkz</p>
+                  </a>
+                </div>
+                <div className="info-group">
+                  <p className="center single-line">
+                    👍 👍🏻 👍🏼 👍🏽 👍🏾 👍🏿 👍 👍🏻 👍🏼 👍🏽 👍🏾 👍🏿 👍 👍🏻 👍🏼 👍🏽 👍🏾 👍🏿 👍 👍🏻 👍🏼 👍🏽 👍🏾 👍🏿 👍 👍🏻 👍🏼 👍🏽 👍🏾 👍🏿 👍 👍🏻 👍🏼 👍🏽 👍🏾 👍🏿
+                  </p>
+                  <p> like us on the fbs </p>
+                  <a href="https://www.facebook.com/wunderwerkz">
+                    <p>/wunderwerkz</p>
+                  </a>
+                </div>  
+                <div className="info-group">
+                  <p className="center single-line"> 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎</p>
+                  <p> get behanced </p>
+                  <a href="https://www.behance.net/wunder_werkz">
+                    <p>/wunder_werkz</p>
+                  </a>
+                </div>
           </ScrollText>
             </div>
           </div>
@@ -44,5 +79,29 @@ class ContactPage extends React.Component {
     );
   }
 }
+//
+// <p className="center single-line"> 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍 📍</p>
+// <a href="https://www.google.com/maps/place/3515+Ringsby+Ct,+Denver,+CO+80216/@39.7733508,-104.9828953,17z/data=!3m1!4b1!4m5!3m4!1s0x876c7903ec2bc8b1:0x5782fe9631c8fd61!8m2!3d39.7733508!4d-104.9807066?sa=X&ved=0ahUKEwitgtycvITWAhVT6Z8KHRlVA5EQ8gEIJzAA"
+//    className="center">
+//   <p> 3515 Ringsby Ct #308 <br/> DENVER, CO 80216 </p>
+// </a>
+// <p className="center single-line"> 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫 📫</p>
+// <a href="mailto:hello@iheartwunderwerkz.com">
+//   <p> hello@iheartwunderwerkz.com </p>
+// </a>
+// <p className="center single-line">  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞  📞</p>
+// <p>303.594.4990</p>
+// <p className="center single-line"> 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 📷 </p>
+// <a href="https://www.instagram.com/wunder_werkz/">
+//   <p>@wunder_werkz</p>
+// </a>
+// <p className="center single-line"> 👍 👍🏻 👍🏼 👍🏽 👍🏾 👍🏿 👍 👍🏻 👍🏼 👍🏽 👍🏾 👍🏿 👍 👍🏻 👍🏼 👍🏽 👍🏾 👍🏿 👍 👍🏻 👍🏼 👍🏽 👍🏾 👍🏿 👍 👍🏻 👍🏼 👍🏽 👍🏾 👍🏿 👍 👍🏻 👍🏼 👍🏽 👍🏾 👍🏿  </p>
+// <a href="https://www.facebook.com/wunderwerkz">
+//   <p>/wunderwerkz</p>
+// </a>
+// <p className="center single-line"> 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎 📎</p>
+// <a href="https://www.behance.net/wunder_werkz">
+//   <p>/wunder_werkz</p>
+// </a>
 
 export default ContactPage;
