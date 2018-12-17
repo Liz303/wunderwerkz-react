40e7cd9d248b7ee7529c275946b74d33c69cc2a3
Liz Henderson <lhenderson303@gmail.com>
2018-12-17 15:12:19 -0700

    updates to some scrolling, cursors, deck link and contact list

--- src/components/pages/AboutPage.js
+++ src/components/pages/AboutPage.js
@@ -1,55 +1,67 @@
-import React from 'react';
-import { browserHistory } from 'react-router';
-import HalfText from '../HalfText.jsx';
-import ScrollText from '../ScrollText.jsx';
-import Navigation from '../Navigation.jsx';
+import React from "react";
+import { browserHistory } from "react-router";
+import HalfText from "../HalfText.jsx";
+import ScrollText from "../ScrollText.jsx";
+import Navigation from "../Navigation.jsx";
 
 class AboutPage extends React.Component {
   render() {
     return (
       <Navigation one="about" two="work" three="contact" four="root">
         <div className="page">
-           <div className="flex-wrapper about">
-             <div className="flex-half center"
-                  onClick={() => {browserHistory.push('/work');}}>
-               <HalfText
-                 className="cursor-click">
-                  Bad Apples Good Design
-               </HalfText>
-             </div>
-             <div className="flex-half"
-                  onClick={e => e.stopPropagation()}>
-               <ScrollText
-                 className="cursor-scroll">
-                 <p className="center rainbow"> ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏</p>
-                 <p>
-                   Wunder Werkz is a full service design studio based in Denver,
-                   Colorado and filled with design geeks and
-                   pizza 🍕 🍕 🍕 lovers.
-                   We work on both self produced and commissioned projects and
-                   believe in style and simplicity that isn’t afraid to have some
-                   fun 🎉 🎉 🎉.
-                 </p>
-                 <p>
-                   From posters to palaces, typography to tattoo parlors,
-                   barber shops to brothels, if you want to step out of the box and
-                   get a little dangerous then we are the design studio for you.
-                 </p>
-                 <p className="center rainbow"> ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏</p>
-                 <h2> We've Worked With Some Cool Cats: </h2>
-                 <p className="small"> The Source 👌 RiNo Yacht Club 👌 Goldyn 👌 New Belgium Brewery 👌
-                    Line Skis 👌 South of France 👌 Among the Colors 👌
-                   Zeppelin Development 👌 34 degrees 👌 Dynia Architects 👌 Under Study 👌 Oso 👌
-                   Track Architecture 👌 Davis Graham & Stubbs 👌 Waves Zine 👌 Bun Bun 👌 Comal 👌
-                   Wurstküche 👌 Cart Driver 👌 Station 16 </p>
-             </ScrollText>
-             </div>
-             <div className="absolute-button cursor-download">
-               <a href="../images/about/Wunderwerkz_capabilities.pdf" download>
-                 <button> The Deck </button>
-               </a>
-             </div>
-           </div>
+          <div className="flex-wrapper about">
+            <div
+              className="flex-half center"
+              onClick={() => {
+                browserHistory.push("/work");
+              }}
+            >
+              <HalfText className="cursor-click">
+                Bad Apples Good Design
+              </HalfText>
+            </div>
+            <div className="flex-half" onClick={e => e.stopPropagation()}>
+              <ScrollText className="cursor-scroll">
+                <p className="center rainbow">
+                  {" "}
+                  ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏
+                </p>
+                <p>
+                  Wunder Werkz is a full service design studio based in Denver,
+                  Colorado and filled with design geeks and pizza 🍕 🍕 🍕
+                  lovers. We work on both self produced and commissioned
+                  projects and believe in style and simplicity that isn’t afraid
+                  to have some fun 🎉 🎉 🎉.
+                </p>
+                <p>
+                  From posters to palaces, typography to tattoo parlors, barber
+                  shops to brothels, if you want to step out of the box and get
+                  a little dangerous then we are the design studio for you.
+                </p>
+                <p className="center rainbow">
+                  {" "}
+                  ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏ ✏
+                </p>
+                <h2> We've Worked With Some Cool Cats: </h2>
+                <p className="small">
+                  The Source Hotel + Market Hall 👌 New Belgium 👌 Zeppelin
+                  Station 👌 Safta 👌 Big Trouble 👌 Niceland 👌 DVLP DNVR 👌
+                  Station 16 👌 Cart Driver 👌 OSO 👌 Winter session 👌
+                  Wurstkütche 👌 Dynia Architects 👌 Madison House 👌 Shake
+                  Shack 👌 Open Desk 👌 Skingraft 👌 Hrim 👌 Goodr 👌 Poketo 👌
+                  Yacht Club 👌 Comal
+                </p>
+              </ScrollText>
+            </div>
+            <div className="absolute-button cursor-download">
+              <a
+                href="../styles/images/about/Wunderwerkz_capabilities.pdf"
+                download
+              >
+                <button> The Deck </button>
+              </a>
+            </div>
+          </div>
         </div>
       </Navigation>
     );
--- src/components/pages/ContactPage.js
+++ src/components/pages/ContactPage.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { browserHistory } from 'react-router';
-import HalfText from '../HalfText.jsx';
-import ScrollText from '../ScrollText.jsx';
-import Navigation from '../Navigation.jsx';
+import React from "react";
+import { browserHistory } from "react-router";
+import HalfText from "../HalfText.jsx";
+import ScrollText from "../ScrollText.jsx";
+import Navigation from "../Navigation.jsx";
 
 class ContactPage extends React.Component {
   render() {
@@ -10,55 +10,63 @@ class ContactPage extends React.Component {
       <Navigation one="contact" two="root" three="about" four="work">
         <div className="page contact">
           <div className="flex-wrapper about">
-            <div className="flex-half center"
-                 onClick={() => { browserHistory.push('/'); }}>
-              <HalfText
-                className="cursor-click">
-                 Reach Out and Touch Us
+            <div
+              className="flex-half center"
+              onClick={() => {
+                browserHistory.push("/");
+              }}
+            >
+              <HalfText className="cursor-click">
+                Reach Out and Touch Us
               </HalfText>
             </div>
-            <div className="flex-half center"
-                 onClick={e => e.stopPropagation()}>
-              <ScrollText
-                className="cursor-scroll">
-                <div className="info-group">
-                  <p className="left"> ✌ </p>
-                  <p className="left">
-                    Stop By <br/>
-                    <a href="https://www.google.com/maps/place/3515+Ringsby+Ct,+Denver,+CO+80216/@39.7733508,-104.9828953,17z/data=!3m1!4b1!4m5!3m4!1s0x876c7903ec2bc8b1:0x5782fe9631c8fd61!8m2!3d39.7733508!4d-104.9807066?sa=X&ved=0ahUKEwitgtycvITWAhVT6Z8KHRlVA5EQ8gEIJzAA"
-                       className="center">
-                      3515 Ringsby Ct #308 <br/> DENVER, CO 80216
-                    </a>
-                  </p>
-                </div>
+            <div
+              className="flex-half center"
+              onClick={e => e.stopPropagation()}
+            >
+              <ScrollText className="cursor-scroll">
+                <div className="info-wrapper">
+                  <div className="info-group">
+                    <p className="left"> ✌ </p>
+                    <p className="left">
+                      Stop By <br />
+                      <a
+                        href="https://www.google.com/maps/place/3515+Ringsby+Ct,+Denver,+CO+80216/@39.7733508,-104.9828953,17z/data=!3m1!4b1!4m5!3m4!1s0x876c7903ec2bc8b1:0x5782fe9631c8fd61!8m2!3d39.7733508!4d-104.9807066?sa=X&ved=0ahUKEwitgtycvITWAhVT6Z8KHRlVA5EQ8gEIJzAA"
+                        className="center"
+                      >
+                        3515 Ringsby Ct #308 <br /> DENVER, CO 80216
+                      </a>
+                    </p>
+                  </div>
 
-                <div className="info-group">
-                  <p className="left"> 📫 </p>
-                  <p className="left">
-                    send a note <br/>
-                    <a href="mailto:hello@iheartwunderwerkz.com">
-                      hello@iheartwunderwerkz.com
-                    </a>
-                  </p>
-                </div>
+                  <div className="info-group">
+                    <p className="left"> 📫 </p>
+                    <p className="left">
+                      send a note <br />
+                      <a href="mailto:hello@iheartwunderwerkz.com">
+                        hello@iheartwunderwerkz.com
+                      </a>
+                    </p>
+                  </div>
 
-                <div className="info-group">
-                  <p className="left"> 📟 </p>
-                  <p className="left">
-                    drop a line <br/>
-                    303.594.4990
-                  </p>
+                  <div className="info-group">
+                    <p className="left"> 📟 </p>
+                    <p className="left">
+                      drop a line <br />
+                      303.594.4990
+                    </p>
+                  </div>
+                  <div className="info-group">
+                    <p className="left"> 📷 </p>
+                    <p className="left">
+                      peep the grams <br />
+                      <a href="https://www.instagram.com/wunder_werkz/">
+                        @wunder_werkz
+                      </a>
+                    </p>
+                  </div>
                 </div>
-                <div className="info-group">
-                  <p className="left"> 📷  </p>
-                  <p className="left">
-                    peep the grams <br/>
-                    <a href="https://www.instagram.com/wunder_werkz/">
-                      @wunder_werkz
-                    </a>
-                  </p>
-                </div>
-          </ScrollText>
+              </ScrollText>
             </div>
           </div>
         </div>
